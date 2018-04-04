56c7d853e95a589addab66cb2b2f3ef8d2d08ee8
ninjha01@gmail.com <ninjha01@gmail.com>
2018-04-04 14:16:28 -0400

    restructuring and added CTCI questions

---
+++ questions/glassdoor/bitstring_glass_1.py
@@ -0,0 +1,49 @@
+class Node():
+    def __init__(self, payload="", parent=None):
+        self.payload = payload
+        self.parent = parent
+
+    def __str__(self):
+        return "{Payload: " + self.payload + " }"
+
+    def __repr__(self):
+        return self.__str__()
+
+def push_to_all(nodes, x):
+    new_nodes = []
+    for node in nodes:
+        new_node = Node(node.payload + x, node.parent)
+        new_nodes.append(new_node)
+    return new_nodes
+
+def split_and_init(nodes):
+    new_nodes = []
+    for node in nodes:
+        left = Node("0", node)
+        right = Node("1", node)
+        new_nodes.append(left)
+        new_nodes.append(right)
+    return new_nodes
+
+def get_string(leaf):
+    curr = leaf.parent
+    result = leaf.payload
+    while curr is not None:
+        result = curr.payload + result
+        curr = curr.parent
+    return(result)
+    
+def main():
+    root = Node()
+    curr_nodes = []
+    curr_nodes.append(root)
+    input = input("Enter a bitstring: \n") or "????"
+
+    for x in input:
+        if(x == '0' or x == '1'):
+            curr_nodes = push_to_all(curr_nodes, x)
+        elif(x == '?'):
+            curr_nodes = split_and_init(curr_nodes)
+        
+    for node in curr_nodes:
+        print(get_string(node))
---
+++ questions/glassdoor/glassdoor.md
@@ -0,0 +1,4 @@
+1. A string consists of ‘0’, ‘1’ and '?'. The question mark can be either '0' or '1'. Find all possible combinations for a string.
+2. Give you a text file, remove duplicated lines.
+3. How can you determine the height of a tree structure? What is the Big O Complexity of the algorithm? Will your answer change if you don't have to get an exact result? What is the tradeoff? 
+3. For a two-dimensional array of arbitrary size (not guaranteed to be square) containing binary values, find the total number of rectangles defined by four corners and parallel to the array's dimensions.
